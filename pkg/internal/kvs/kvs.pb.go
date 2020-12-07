4c81b0f9e775ed9c46fdaca4c1ae1957aa950496
pableeee <pablo.duco@mercadolibre.com>
2020-12-07 18:49:58 -0300

    generates kvs proto code

--- pkg/internal/kvs/kvs.pb.go
+++ pkg/internal/kvs/kvs.pb.go
@@ -7,11 +7,7 @@
 package kvs
 
 import (
-	context "context"
 	proto "github.com/golang/protobuf/proto"
-	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -340,7 +336,7 @@ type GetResponse struct {
 	unknownFields protoimpl.UnknownFields
 
 	Key    string    `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
-	Values [][]byte  `protobuf:"bytes,2,rep,name=values,proto3" json:"values,omitempty"`
+	Values []byte    `protobuf:"bytes,2,opt,name=values,proto3" json:"values,omitempty"`
 	Error  *Response `protobuf:"bytes,3,opt,name=error,proto3" json:"error,omitempty"`
 }
 
@@ -383,7 +379,7 @@ func (x *GetResponse) GetKey() string {
 	return ""
 }
 
-func (x *GetResponse) GetValues() [][]byte {
+func (x *GetResponse) GetValues() []byte {
 	if x != nil {
 		return x.Values
 	}
@@ -533,7 +529,7 @@ var file_kvs_proto_rawDesc = []byte{
 	0x6b, 0x65, 0x79, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x22, 0x5c,
 	0x0a, 0x0b, 0x47, 0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x10, 0x0a,
 	0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12,
-	0x16, 0x0a, 0x06, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0c, 0x52,
+	0x16, 0x0a, 0x06, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52,
 	0x06, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x73, 0x12, 0x23, 0x0a, 0x05, 0x65, 0x72, 0x72, 0x6f, 0x72,
 	0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x0d, 0x2e, 0x6b, 0x76, 0x73, 0x2e, 0x52, 0x65, 0x73,
 	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x52, 0x05, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x22, 0x34, 0x0a, 0x0a,
@@ -718,156 +714,4 @@ func file_kvs_proto_init() {
 	file_kvs_proto_rawDesc = nil
 	file_kvs_proto_goTypes = nil
 	file_kvs_proto_depIdxs = nil
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConnInterface
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
-
-// KVSServiceClient is the client API for KVSService service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type KVSServiceClient interface {
-	Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error)
-	Put(ctx context.Context, in *PutRequest, opts ...grpc.CallOption) (*Response, error)
-	Del(ctx context.Context, in *DelRequest, opts ...grpc.CallOption) (*Response, error)
-}
-
-type kVSServiceClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewKVSServiceClient(cc grpc.ClientConnInterface) KVSServiceClient {
-	return &kVSServiceClient{cc}
-}
-
-func (c *kVSServiceClient) Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error) {
-	out := new(GetResponse)
-	err := c.cc.Invoke(ctx, "/kvs.KVSService/Get", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *kVSServiceClient) Put(ctx context.Context, in *PutRequest, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
-	err := c.cc.Invoke(ctx, "/kvs.KVSService/Put", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *kVSServiceClient) Del(ctx context.Context, in *DelRequest, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
-	err := c.cc.Invoke(ctx, "/kvs.KVSService/Del", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// KVSServiceServer is the server API for KVSService service.
-type KVSServiceServer interface {
-	Get(context.Context, *GetRequest) (*GetResponse, error)
-	Put(context.Context, *PutRequest) (*Response, error)
-	Del(context.Context, *DelRequest) (*Response, error)
-}
-
-// UnimplementedKVSServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedKVSServiceServer struct {
-}
-
-func (*UnimplementedKVSServiceServer) Get(context.Context, *GetRequest) (*GetResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
-}
-func (*UnimplementedKVSServiceServer) Put(context.Context, *PutRequest) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Put not implemented")
-}
-func (*UnimplementedKVSServiceServer) Del(context.Context, *DelRequest) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Del not implemented")
-}
-
-func RegisterKVSServiceServer(s *grpc.Server, srv KVSServiceServer) {
-	s.RegisterService(&_KVSService_serviceDesc, srv)
-}
-
-func _KVSService_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(KVSServiceServer).Get(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/kvs.KVSService/Get",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(KVSServiceServer).Get(ctx, req.(*GetRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _KVSService_Put_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(PutRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(KVSServiceServer).Put(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/kvs.KVSService/Put",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(KVSServiceServer).Put(ctx, req.(*PutRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _KVSService_Del_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DelRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(KVSServiceServer).Del(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/kvs.KVSService/Del",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(KVSServiceServer).Del(ctx, req.(*DelRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _KVSService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "kvs.KVSService",
-	HandlerType: (*KVSServiceServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "Get",
-			Handler:    _KVSService_Get_Handler,
-		},
-		{
-			MethodName: "Put",
-			Handler:    _KVSService_Put_Handler,
-		},
-		{
-			MethodName: "Del",
-			Handler:    _KVSService_Del_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "kvs.proto",
 }
